9895da91a32fac508339404cb246fb75afe76c7b
rishabhkeshan <rishabhkeshan@gmail.com>
2023-01-22 18:47:50 +0530

    added codemirror ide

--- src/pages/HomeScreen/HomeScreen.js
+++ src/pages/HomeScreen/HomeScreen.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
 
 export default function HomeScreen() {
+      const onChange = React.useCallback((value, viewUpdate) => {
+        console.log("value:", value);
+      }, []);
   return (
-    <div>HomeScreen</div>
-  )
+    <>
+      <div>Sorobix IDE</div>
+      <CodeMirror
+        value="console.log('hello world!');"
+        height="200px"
+        theme="dark"
+        extensions={[javascript({ jsx: true })]}
+        onChange={onChange}
+      />
+    </>
+  );
 }
