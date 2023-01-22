19d4b2b63e871a3c9fe228bf1104094fd4bfdba9
rishabhkeshan <rishabhkeshan@gmail.com>
2023-01-22 19:28:29 +0530

    added theme

--- src/pages/HomeScreen/HomeScreen.js
+++ src/pages/HomeScreen/HomeScreen.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import "./HomeScreen.scss";
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
+import Sorobix from "../../assets/Sorobix_Logo.svg";
 
 export default function HomeScreen() {
       const onChange = React.useCallback((value, viewUpdate) => {
         console.log("value:", value);
       }, []);
   return (
-    <>
-      <div>Sorobix IDE</div>
+    <article className="homescreen">
+      <section className="homescreen_header">
+        <div className="homescreen_header_logo">
+          <img className="homescreen_header_logo_image" src={Sorobix} alt="sorobix" />
+        </div>
+      </section>
       <CodeMirror
         value="console.log('hello world!');"
         height="200px"
@@ -16,6 +22,6 @@ export default function HomeScreen() {
         extensions={[javascript({ jsx: true })]}
         onChange={onChange}
       />
-    </>
+    </article>
   );
 }
