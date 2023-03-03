5c0d6de0743444dac25d33e640228c8c61e3496e
rishabhkeshan <rishabhkeshan@gmail.com>
2023-03-03 18:54:16 +0530

    multi account selection UI, added function invoking, G/S generation

---
+++ src/pages/IDEScreen/DropDown/DropDown.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import "./DropDown.scss";
+import Acc1 from "../../../assets/account1pfp.svg";
+
+function DropDown({sortBy, setSortBy}) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="relative w-full">
+      <div
+        className={`dropdown-button relative ${open ? "" : ""}`}
+        onClick={() => setOpen(!open)}
+      >
+        <div className="dropdown-inner">
+          <div className="IDEScreen_maincontainer_sidebar_outercontainer_bottomcontainer_executecontainer_accountscontainer_image">
+            <img src={Acc1} alt="account" />
+          </div>
+          <div className="IDEScreen_maincontainer_sidebar_outercontainer_bottomcontainer_executecontainer_accountscontainer_separator"></div>
+          <div className="IDEScreen_maincontainer_sidebar_outercontainer_bottomcontainer_executecontainer_accountscontainer_accountname">
+            {sortBy}
+          </div>
+        </div>
+        {/* <span className="mr-4">
+          {sortBy === "inProgress" ? "In Progress" : sortBy}
+        </span> */}
+
+        <svg
+          className="w-2 h-2 text-white dark:text-white absolute dropdown-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="#D99BFF"
+          style={{paddingRight:"16px"}}
+          height={"24px"}
+        >
+          <path
+            fill-rule="evenodd"
+            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </div>
+      {open && (
+        <div className="dropdown-modal">
+          <p
+            className="option"
+            onClick={() => {
+              setSortBy("Account 1");
+              setOpen(false);
+            }}
+          >
+            Account 1
+          </p>
+          <p
+            className="option"
+            onClick={() => {
+              setSortBy("Account 2");
+              setOpen(false);
+            }}
+          >
+            Account 2
+          </p>
+          <p
+            className="option"
+            onClick={() => {
+              setSortBy("Account 3");
+              setOpen(false);
+            }}
+          >
+            Account 3
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default DropDown;
--- src/pages/IDEScreen/IDEScreen.js
+++ src/pages/IDEScreen/IDEScreen.js
@@ -2,14 +2,22 @@ import React, { useState } from "react";
 import "./IDEScreen.scss";
 import CodeMirror from "@uiw/react-codemirror";
 import { rust } from "@codemirror/lang-rust";
-import Sorobix from "../../assets/Sorobix_Logo.svg";
+import Acc1 from "../../assets/account1pfp.svg";
 import TextField from "@mui/material/TextField";
-import plus_icon from "../../assets/plus_icon.svg";
+import plus from "../../assets/plus.svg";
 import Api from "../../utils/api";
 import { useSnackbar } from "notistack";
 import Loader from "../../components/Loader/Loader";
+import DropDown from "./DropDown/DropDown";
+
 export default function IDEScreen() {
-      const [code, setCode] = useState(`#![no_std]
+  const [sortBy, setSortBy] = useState("Account 1");
+  const [functionKeys, setFunctionKeys] = useState(["","","",""]);
+    const [functionValues, setFunctionValues] = useState(["","","","",""]);
+  const [funcState, setFuncState] = useState("about");
+  const [contractAddress, setContractAddress] = useState("");
+  const [functionName, setFunctionName] = useState("");
+  const [code, setCode] = useState(`#![no_std]
 use soroban_sdk::{contractimpl, symbol, vec, Env, Symbol, Vec};
 
 pub struct Contract;
@@ -25,20 +33,199 @@ impl Contract {
   }, []);
   return (
     <article className="IDEScreen">
-      <section className="IDEScreen_header">
-        <div className="IDEScreen_header_logo">
-          <img
-            className="IDEScreen_header_logo_image"
-            src={Sorobix}
-            alt="sorobix"
-          />
-        </div>
-      </section>
       <section className="IDEScreen_maincontainer">
         <div className="IDEScreen_maincontainer_sidebar">
-          <div className="IDEScreen_maincontainer_sidebar_maincontainer">
-            <div className="IDEScreen_maincontainer_sidebar_maincontainer_title">Welcome to Sorobix</div>
-            <div className="IDEScreen_maincontainer_sidebar_maincontainer_text">Start your Stellar journey here through our basic snippets and easy to access playground.</div>
+          <div className="IDEScreen_header">
+            <div className="IDEScreen_header_logo">SOROBIX IDE</div>
+          </div>
+          <div className="IDEScreen_maincontainer_sidebar_outercontainer">
+            <div className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer">
+              <div className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_bar">
+                <div
+                  onClick={() => {
+                    setFuncState("about");
+                  }}
+                  className={`IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_bar_left ${
+                    funcState === "about" ? "activeBG" : ""
+                  }`}
+                >
+                  About
+                </div>
+                <div
+                  onClick={() => {
+                    setFuncState("function");
+                  }}
+                  className={`IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_bar_right ${
+                    funcState === "function" ? "activeBG" : ""
+                  }`}
+                >
+                  Functions
+                </div>
+              </div>
+              {funcState === "about" && (
+                <div className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_aboutcontainer">
+                  Sorobix is your beginner friendly playground to try out
+                  Soroban contracts on the fly.
+                </div>
+              )}
+              {funcState === "function" && (
+                <div className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_functioncontainer">
+                  <div className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_functioncontainer_inputbox">
+                    <div className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_functioncontainer_inputbox_title">
+                      Deployed Contract Address
+                    </div>
+                    <div className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_functioncontainer_inputbox_value">
+                      <input
+                        className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_functioncontainer_inputbox_value_input"
+                        placeholder="Enter Contract Address"
+                        value={contractAddress}
+                        onChange={(e) => {
+                          setContractAddress(e.target.value);
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <div className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_functioncontainer_inputbox">
+                    <div className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_functioncontainer_inputbox_title">
+                      Function Name
+                    </div>
+                    <div className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_functioncontainer_inputbox_value">
+                      <input
+                        className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_functioncontainer_inputbox_value_input"
+                        placeholder="Enter Function Name"
+                        value={functionName}
+                        onChange={(e) => {
+                          setFunctionName(e.target.value);
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <div className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_functioncontainer_inputbox">
+                    <div className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_functioncontainer_inputbox_title">
+                      <div>Function Parameters</div>
+                      <div
+                      className="pointer"
+                        onClick={() => {
+                          setFunctionKeys([...functionKeys, ""]);
+                          setFunctionValues([...functionValues,""]);
+                        }}
+                      >
+                        <img className="pointer" src={plus} alt="plus icon"/>
+                      </div>
+                    </div>
+                    <div className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_functioncontainer_inputbox_table">
+                      <div className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_functioncontainer_inputbox_table_row">
+                        <div className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_functioncontainer_inputbox_table_row_l">
+                          Key
+                        </div>
+                        <div className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_functioncontainer_inputbox_table_row_r">
+                          Value
+                        </div>
+                      </div>
+                      <div className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_functioncontainer_inputbox_table_height">
+                        {functionKeys.map((value, index) => {
+                          return (
+                            <div className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_functioncontainer_inputbox_table_row">
+                              <div className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_functioncontainer_inputbox_table_row_l">
+                                <input
+                                  value={functionKeys[index]}
+                                  placeholder={`Enter Key ${index + 1}`}
+                                  onChange={(e) =>
+                                    setFunctionKeys(
+                                      functionKeys.map((el, i) =>
+                                        i === index ? e.target.value : el
+                                      )
+                                    )
+                                  }
+                                  className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_functioncontainer_inputbox_table_row_l_input"
+                                />
+                              </div>
+                              <div className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_functioncontainer_inputbox_table_row_r">
+                                <input
+                                  value={functionValues[index]}
+                                  placeholder={`Enter Value ${index + 1}`}
+                                  onChange={(e) =>
+                                    setFunctionValues(
+                                      functionValues.map((el, i) =>
+                                        i === index ? e.target.value : el
+                                      )
+                                    )
+                                  }
+                                  className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_functioncontainer_inputbox_table_row_r_input"
+                                />
+                              </div>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_functioncontainer_invokebutton">
+                    Invoke Contract
+                  </div>
+                  {/* <div className="IDEScreen_maincontainer_sidebar_outercontainer_topcontainer_functioncontainer"></div> */}
+                </div>
+              )}
+            </div>
+            <div className="IDEScreen_maincontainer_sidebar_outercontainer_bottomcontainer">
+              <div className="IDEScreen_maincontainer_sidebar_outercontainer_bottomcontainer_bar">
+                Execution Center
+              </div>
+              <div className="IDEScreen_maincontainer_sidebar_outercontainer_bottomcontainer_executecontainer">
+                {/* <div className="IDEScreen_maincontainer_sidebar_outercontainer_bottomcontainer_executecontainer_accountscontainer">
+                  <div className="IDEScreen_maincontainer_sidebar_outercontainer_bottomcontainer_executecontainer_accountscontainer_image">
+                    <img src={Acc1} alt="account" />
+                  </div>
+                  <div className="IDEScreen_maincontainer_sidebar_outercontainer_bottomcontainer_executecontainer_accountscontainer_separator"></div>
+                  <div className="IDEScreen_maincontainer_sidebar_outercontainer_bottomcontainer_executecontainer_accountscontainer_accountname">
+                    Account 1
+                  </div>
+                </div> */}
+                <div className="w-full">
+                  <DropDown sortBy={sortBy} setSortBy={setSortBy} />
+                </div>
+                <div className="IDEScreen_maincontainer_sidebar_outercontainer_bottomcontainer_executecontainer_generatecontainer">
+                  <div className="IDEScreen_maincontainer_sidebar_outercontainer_bottomcontainer_executecontainer_generatecontainer_generatebutton">
+                    Generate G/S Keys
+                  </div>
+                </div>
+                <div className="IDEScreen_maincontainer_sidebar_outercontainer_bottomcontainer_executecontainer_keycontainer">
+                  <div
+                    id="gkey"
+                    className="IDEScreen_maincontainer_sidebar_outercontainer_bottomcontainer_executecontainer_keycontainer_title"
+                  >
+                    G Key
+                  </div>
+                  <div
+                    id="gvalue"
+                    className="IDEScreen_maincontainer_sidebar_outercontainer_bottomcontainer_executecontainer_keycontainer_value"
+                  >
+                    GDHMW6QZOL73SHKG2JA3YHXFDS5ZRW....
+                  </div>
+                  <div
+                    id="skey"
+                    className="IDEScreen_maincontainer_sidebar_outercontainer_bottomcontainer_executecontainer_keycontainer_title"
+                  >
+                    S Key
+                  </div>
+                  <div
+                    id="svalue"
+                    className="IDEScreen_maincontainer_sidebar_outercontainer_bottomcontainer_executecontainer_keycontainer_value"
+                  >
+                    GDHMW6QZOL73SHKG2JA3YHXFDSS5ZRW....
+                  </div>
+                </div>
+                <div className="IDEScreen_maincontainer_sidebar_outercontainer_bottomcontainer_executecontainer_buttonscontainer">
+                  <div className="IDEScreen_maincontainer_sidebar_outercontainer_bottomcontainer_executecontainer_buttonscontainer_compile">
+                    Compile Contract
+                  </div>
+                  <div className="IDEScreen_maincontainer_sidebar_outercontainer_bottomcontainer_executecontainer_buttonscontainer_deploy">
+                    Deploy Contract
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="IDEScreen_maincontainer_innercontainer">
@@ -52,10 +239,10 @@ impl Contract {
             />
           </div>
           <div className="IDEScreen_maincontainer_innercontainer_outputcontainer">
-            <div className="IDEScreen_maincontainer_innercontainer_outputcontainer_title">
+            <div className="IDEScreen_maincontainer_innercontainer_outputcontainer_titlecontainer">
               Output
             </div>
-            <div className="IDEScreen_maincontainer_innercontainer_outputcontainer_text">{`>`}</div>
+            <div className="IDEScreen_maincontainer_innercontainer_outputcontainer_textcontainer">{`>`}</div>
           </div>
         </div>
       </section>
